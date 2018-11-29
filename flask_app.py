d1adfaee0375a01f9f760a3c1496da4b3c623cb2
Junel Maglunsod <junelmaglunsod0256@gmail.com>
2018-11-30 00:26:01 +0800

    upload

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import *
+import os
 
 
 
 
-@app.route('/',methods=['GET'])
+
+
+@app.route('/',methods=['GET', 'POST'])
 def index():
 	coords = Landmarks.query.all()
 	return render_template("home.html",coords = coords, msg="hello")
@@ -14,6 +17,7 @@ def setpoints():
 	coords = Landmarks.query.all()
 	if request.method == "POST":
 		namex  = request.form['name']
+
 		pointsx = request.form['points']
 		type_x = request.form['type']
 		
@@ -23,63 +27,53 @@ def setpoints():
 		coords = Landmarks.query.all()
 		return render_template('home.html',coords = coords, msg="hello")
 	
-	return render_template('setpoint.html')
+	return render_template('setpoints.html')
+
+
+
+@app.route('/activateUpdateMap', methods=['GET'])
+def activateUpdateMap():
+	os.system('python updateFloors.py')
+
+@app.route('/upload', methods=['GET'])
+def activate():
+	return render_template("upload.html")
 
 @app.route('/map',methods=['GET'])
 def map():
-	return render_template("setpoint.html")
+	return render_template("map.html")
 
-@app.route('/coet',methods=['GET'])
-def coet():
-	return render_template("coet.html")
+@app.route('/landmark/<id>',methods=['GET'])
+def landmark(id):
+	land = Landmarks.query.filter_by(land_id=id).first()
+	college = land.name
+	return render_template("landmark.html", college=college)
 
-@app.route('/ced',methods=['GET'])
-def ced():
-	return render_template("ced.html")
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-@app.route('/coet_con',methods=['GET'])
-def coet_con():
-	return render_template("coet_con.html")
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'C:\Users\Admin\Desktop\upload\static')
+    print(target)
 
-@app.route('/cbaa',methods=['GET'])
-def cbaa():
-	return render_template("cbaa.html")
+    if not os.path.isdir(target):
+        os.mkdir(target)
 
-@app.route('/csm',methods=['GET'])
-def csm():
-	return render_template("csm.html")
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+  
+    return render_template("complete.html")
 
-@app.route('/gym',methods=['GET'])
-def gym():
-	return render_template("gym.html")
+@app.route("/update", methods=['GET'])
+def update():
+    return render_template("upload.html")
 
-@app.route('/scs',methods=['GET'])
-def scs():
-	return render_template("scs.html")
 
-@app.route('/ids',methods=['GET'])
-def ids():
-	return render_template("ids.html")
 
-@app.route('/lawn',methods=['GET'])
-def lawn():
-	return render_template("lawn.html")
-
-@app.route('/admin',methods=['GET'])
-def admin():
-	return render_template("admin.html")
-
-@app.route('/cass',methods=['GET'])
-def cass():
-	return render_template("cass.html")
-
-@app.route('/cbaa_extension',methods=['GET'])
-def cbaa_extension():
-	return render_template("cbaa_extension.html")
-
-@app.route('/clinic',methods=['GET'])
-def clinic():
-	return render_template("clinic.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- models.py
+++ models.py
@@ -2,17 +2,42 @@ import sqlalchemy
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import os
 from flask_sqlalchemy import SQLAlchemy
-# from flask_login import *
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://postgres:postgres@localhost/map"
+app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://postgres:imapkuno@localhost/map1"
 app.config['SECRET_KEY'] = 'veryhard'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['USE_SESSION_FOR_NEXT'] = True
 app.secret_key = os.urandom(24)
 app.debug = True
 db = SQLAlchemy(app)
+
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
 
 
 class Landmarks(db.Model):
@@ -25,5 +50,38 @@ class Landmarks(db.Model):
 		self.name = name
 		self.points = points
 		self.type_ = type_
+
+
+
+class Rooms(db.Model):
+	room_id = db.Column(db.Integer, primary_key = True)
+	floor_id = db.Column(db.Integer, db.ForeignKey('floors.floor_id'))
+	room_num = db.Column(db.String(10), unique = True)
+
+
+	def __init__(self, floor_id, room_num):
+		self.floor_num = floor_num
+		self.room_num = room_num
+
+class Floors(db.Model):
+	floor_id = db.Column(db.Integer, primary_key = True)
+	floor_landmark = db.Column(db.String(10), unique = True)
+	rf_id = db.relationship("Rooms", uselist =False, backref = "floor_room")
+
+
+	def __init__(self, floor_num, room_num):
+		self.floor_num = floor_num
+		self.room_num = room_num
+
+class Roommarks(db.Model):
+	room_id =  db.Column(db.Integer, primary_key=True)
+	room_name = db.Column(db.String(50), unique=True)
+	points = db.Column(db.String(200), unique=True)
+	floor_num= db.Column(db.String(100))
+
+	def __init__(self, room_name, points, floor_num):
+		self.room_name = room_name
+		self.points = points
+		self.floor_num = floor_num
 
 db.create_all()
